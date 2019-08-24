be77cd65a6ae98182821b0aeb9e2bdc993f6fc33
Minxrod <minxrod@gmail.com>
2019-08-23 21:37:02 -0400

    variable map size, rand fix

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <switch.h>
 
@@ -17,8 +18,11 @@ void init_map();
 void init_cells();
 void update_all_cells();
 void draw_all_cells();
+void draw_info();
+
 void swap_offset();
 
+void user_input(u64 key);
 int sum_cells_in_area(int x, int y);
 
 void set_pixel(int x, int y, int rgba8888);
@@ -32,6 +36,8 @@ int map_scale;  //zoom on screen
 int map_offset; //which half of array is being written to
 
 int cells[MAX_MAP_SIZE * 2]; //max map size * 2
+
+int frame; //current generation
 
 //stuff I don't really understand
 NWindow* win;
@@ -47,6 +53,8 @@ int main(int argc, char* argv[])
 	framebufferCreate(&fb, win, FB_WIDTH, FB_HEIGHT, PIXEL_FORMAT_RGBA_8888, 2);
 	framebufferMakeLinear(&fb);	
 	
+	consoleInit(NULL);
+	
 	init_map();
 	init_cells();
 	
@@ -54,20 +62,60 @@ int main(int argc, char* argv[])
     {
         hidScanInput();
 
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        u64 kDown = hidKeysHeld(CONTROLLER_P1_AUTO);
 
         if (kDown & KEY_PLUS)
             break; 
 		
+		user_input(kDown);
+		
 		update_all_cells();
 		draw_all_cells();
-		swap_offset(); //flip array
+		//draw_info();
 		
+		swap_offset(); //flip array offset
+		frame++;
     }
 
+    consoleExit(NULL);
 	framebufferClose(&fb);
 	
     return 0;
+}
+
+//process user button input
+void user_input(u64 key)
+{
+	if (key & KEY_DOWN)
+	{
+		if ((map_height + 1) * map_scale < MAX_HEIGHT)
+			map_height++;
+	}
+	
+	if (key & KEY_UP)
+	{
+		if (map_height > 0)
+			map_height--;
+	}
+	
+	if (key & KEY_LEFT)
+	{
+		if (map_width > 0)
+			map_width--;
+	}
+	
+	if (key & KEY_RIGHT)
+	{
+		if ((map_width + 1) * map_scale < MAX_WIDTH)
+			map_width++;
+	}
+	
+	if (key & KEY_X)
+	{
+		map_scale *= 2;
+		if (map_scale > 8)
+			map_scale = 1;
+	}
 }
 
 void update_all_cells()
@@ -101,6 +149,7 @@ void init_map()
 //requires initialized map before use.
 void init_cells()
 {
+	srand(time(NULL));
 	for (int y = 0; y < map_height; y++)
 	{
 		for (int x = 0; x < map_width; x++)
@@ -150,27 +199,47 @@ void draw_all_cells()
 	framebufferEnd(&fb);
 }
 
+void draw_info()
+{
+	printf("\x1b[0;20HSize: %03i x %03i, Generation: %i", map_width, map_height, frame);
+	consoleUpdate(NULL);
+}
+
 void swap_offset()
 {
 	map_offset = (map_offset == 0) ? MAX_MAP_SIZE : 0;
 }
 
-//sets a pixel in the framebuffer.
+//draws a "pixel" - really a square of pixels depending on the current scale.
 //make sure to initialize framebuffer first...
 void set_pixel(int x, int y, int rgba8888)
 {
-	u32 pos = y * stride / sizeof(u32) + x;	
-	
-	framebuf[pos] = rgba8888;
+	int sx, sy; //scaled x and scaled y
+	for (int i = 0; i < map_scale; i++)
+	{
+		for (int j = 0; j < map_scale; j++)
+		{
+			//finds scaled pixel coordinates
+			sx = x * map_scale + i;
+			sy = y * map_scale + j;
+			
+			//finds position of pixel in buffer
+			u32 pos = sy * stride / sizeof(u32) + sx;	
+			
+			framebuf[pos] = rgba8888;
+		}
+	}
 }
 
 //sets the cell at the given position.
+//NOTE: sets 'current' array half.
 void set_pos(int x, int y, int value)
 {
 	cells[y * MAX_WIDTH + x + map_offset] = value; 
 }
 
 //get cell at position.
+//NOTE: uses 'old' array half.
 int get_pos(int x, int y)
 {
 	return cells[y * MAX_WIDTH + x + (MAX_MAP_SIZE - map_offset)];
